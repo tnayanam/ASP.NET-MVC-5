8f28cb1285837da6d4bdfae0dc91cf5ea8289361
unknown <tnayanam@syr.edu>
2018-04-24 10:34:34 -0400

    Send data to backend when the data property is not part of the model

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -147,7 +147,7 @@ namespace WebApplication1.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Register(RegisterViewModel model, int totalAmount)
         {
             if (ModelState.IsValid)
             {
